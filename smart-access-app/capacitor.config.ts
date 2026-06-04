4164aff66786150494101a40be7091bc814cee92
Phynomo <daniele09099@gmail.com>
2026-06-03 20:36:24 -0600

    ADD: Proyecto angular

---
+++ smart-access-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.phynomo.smartacess',
+  appName: 'SmartAccess',
+  webDir: 'dist/smart-access-app/browser'
+};
+
+export default config;
